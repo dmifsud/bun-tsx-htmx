5cf2b79811534296bc35c477ce3bb37eb89e5145
David Mifsud <david.mifsud@grokacademy.org>
2024-07-19 18:20:33 +0930

    fix: prod build hx helpers

--- src/templates/login/Login.page.tsx
+++ src/templates/login/Login.page.tsx
@@ -27,7 +27,7 @@ const Login = () => {
                             </div>
                             <div className='text-left'>
                                 <label htmlFor="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                <input type="password" name="password" placeholder="••••••••" required hc-invalid-target="#password-error" hc-invalid-class="invalid:border-red-500 invalid:text-red-600" hc-invalid-events="blur"
+                                <input type="password" name="password" placeholder="••••••••" required hc-invalid-target="#password-error" hc-invalid-class="invalid:border-red-500 invalid:text-red-600" hc-invalid-events="blur keyup"
                                     class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                     />
                                      <p id="password-error" className='text-red-600 text-sm'></p>
